a43aad385a9a68e427de8d2fb0873a2d6cda6f5a
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2021-03-17 22:48:29 +0700

    script push notofication

---
+++ push.js
@@ -0,0 +1,16 @@
+var push = require('web-push')
+
+//cara mendapatakan public dan  private key 
+
+/* 
+   jika sudah menjalankan  perintah di bawah ini dengan perintah -node push.js-
+   maka tulis kembali seperti code selanjutnya
+   let vapidKeys =push.generateVAPIDKeys();
+   console.log(vapidKeys) 
+   
+*/
+
+let vapidKeys ={
+  publicKey: 'BL573nj_9EpSn5A39nXhcPQEKLR15V7TBQwTJkZwxUFliIDgX1jhJS8sD7VYLwKeUb5mhdDZ3_ofNi5bWPBzDxI',
+  privateKey: 'xF25SnH8ApUyNvYGUG8Us-IzdEzUqt_ES7NzxgTc9bI'
+}
